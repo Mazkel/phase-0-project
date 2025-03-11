5cc3d9d6e0286740e39d8dc29f425a052dd9306e
Silongo Vailolo <silongo68@hotmail.com>
2025-03-11 10:43:17 +1000

    added eventlistener and if statement to change between two colors.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const h2 = document.createElement('h2');
 h2.id="h2";
 h2.textContent = "This content added by Javascript";
 document.querySelector("body").appendChild(h2);
+
+const originalColor = h2.style.color || "white";
+
+h2.addEventListener("click", function() {
+    if (h2.style.color === "chartreuse") {
+        h2.style.color = originalColor;
+    } else {
+        h2.style.color = "chartreuse";
+    }  
+});
