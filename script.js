c6d3f3f2204715bde11e0d18c35932d20a1e5366
Silongo Vailolo <silongo68@hotmail.com>
2025-03-11 12:00:13 +1000

    Added bio element and p tag using DOM

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ h2.id="h2";
 h2.textContent = "This content added by Javascript";
 document.querySelector("body").appendChild(h2);
 
+document.querySelector('h1').insertAdjacentElement("afterend", h2,)
+
 const originalColor = h2.style.color || "white";
 
 h2.addEventListener("click", function() {
@@ -12,3 +14,12 @@ h2.addEventListener("click", function() {
         h2.style.color = "chartreuse";
     }  
 });
+
+const container = document.querySelector('.container')
+const p = document.createElement('p')
+p.className= "p"
+p.textContent = "Hi, This is my personal website. I've learned HTML, CSS, and JS, and I'm also using DOM to create this sentence."
+container.appendChild(p)
+
+const h3 = container.querySelector('#bio')
+h3.insertAdjacentElement("afterend", p)
